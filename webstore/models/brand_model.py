d921fcd78be0b706cc14f3138d82b0bcdcbf3cea
kkkovalov <vlad_kovalov@icloud.com>
2023-08-21 19:52:25 -0700

    Updated error fixed

--- webstore/models/brand_model.py
+++ webstore/models/brand_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from webstore.models import Category
 
 class Brand(models.Model):
-    name = models.CharField(max_length=255, blank=False, verbose_name='Brand name')g
+    name = models.CharField(max_length=255, blank=False, verbose_name='Brand name')
     slug_name = models.SlugField(max_length=255, blank=False, null=False, verbose_name="Slug name", unique=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TimeField(max_length=1020, verbose_name='Description')
