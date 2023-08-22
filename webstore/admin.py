f1397597e899d83fcab480a477d6ed10b6f015e4
kkkovalov <vlad_kovalov@icloud.com>
2023-08-21 19:45:20 -0700

    finished creating product basic model

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-from webstore.models import Product, Brand, Category, Basket, 
+from webstore.models import Product, Brand, Category, Basket
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug_name": ["name"]}
--- webstore/models/product_model.py
+++ webstore/models/product_model.py
@@ -4,19 +4,19 @@ from django.conf import settings
 from webstore.models import Category, Brand
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, blank=False, verbose_name='Product name',)
-    description = models.TimeField(max_length=1020, verbose_name='Description')
-    price = models.FloatField(blank=False, verbose_name='Price')
-    stock_quantity = models.IntegerField(blank=False, default=0, null=False, verbose_name='In stock')
+    name = models.CharField(max_length=255, verbose_name='Product name', blank=False, null=False)
+    slug_name = models.SlugField(verbose_name='Slug name', max_length=255, blank=False, null=False)
+    type = models.CharField(max_length=255, verbose_name='Type of product', blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Product category')
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name='Product brand')
-    # picture_url = 
+    data = models.JSONField(max_length=255, verbose_name="Custom data", blank=True, null=True)
+    description = models.TimeField(max_length=1020, verbose_name='Description', blank=False, null=False)
+    price = models.FloatField(verbose_name='Price', blank=False, null=False)
+    stock_quantity = models.IntegerField(blank=False,verbose_name='In stock', default=0, null=False)
+    picture_url = models.URLField(max_length=255, verbose_name="Picture URL", blank=True, null=True)
     
     def __str__(self):
         return self.name
     
     def get_current_rating(self):
-        pass
-    
-    def get_pictures(self):
         pass
